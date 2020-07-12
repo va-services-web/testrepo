c49970ab62aff6f58360486a806d6432a8922a05
Valentin Apostu <va.services.web@gmail.com>
2020-07-12 12:36:47 +0200

    add a class for math functions

--- src/compute.py
+++ src/compute.py
@@ -1,1 +1,17 @@
-ttestststs
+
+class Compute:
+    def __init__(self, operator, operands):
+        self.operator = operator
+        self.operands = operands
+
+    def add(self):
+        pass
+
+    def substract(self):
+        pass
+
+    def divide(self):
+        pass
+
+    def multiply(self):
+        pass
